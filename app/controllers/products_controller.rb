33a997c8bf8a83f020c0e02f9cc329f3c2d6ad42
EduardBogoslovtcev2 <ebogoslovtcev@rrc.ca>
2026-04-02 11:33:35 -0500

    added search and filters functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,32 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.includes(:brand).page(params[:page]).per(12)
+    @products = Product.includes(:brand, :layout, :switch_type)
+
+    # Search
+    if params[:search].present?
+      @products = @products.where("name LIKE ?", "%#{params[:search]}%")
+    end
+
+    # Filters
+    if params[:brand_id].present?
+      @products = @products.where(brand_id: params[:brand_id])
+    end
+
+    if params[:layout_id].present?
+      @products = @products.where(layout_id: params[:layout_id])
+    end
+
+    if params[:switch_type_id].present?
+      @products = @products.where(switch_type_id: params[:switch_type_id])
+    end
+
+    # Pagination (keep LAST)
+    @products = @products.page(params[:page]).per(12)
+
+    # For dropdowns
+    @brands = Brand.all
+    @layouts = Layout.all
+    @switch_types = SwitchType.all
   end
 
   def show
